537e32e82d0ca83d0cb0d3dfeb33d7da79dfacbc
Rustam <rfhamzin@gmail.com>
2018-04-01 22:29:07 +0500

    Седьмой commit

---
+++ lesson 5 - hard/project/js/script.js
@@ -0,0 +1,28 @@
+
+let btnOpenShop = document.getElementById('open-btn');
+// let leftMenu = document.getElementsByClassName('')
+
+// Получить поля категории товаров
+var categoryProducts = document.getElementsByClassName('goods-item');
+for ( let i=0; i < categoryProducts.length; i++ ) {
+	var categoryProduct = categoryProducts[i];
+	
+}
+
+// Получить все 4 кнопки
+var btnList = document.getElementsByTagName('button');
+for ( let i=0; i < btnList.length; i++ ) {
+	var btn = btnList[i];
+
+}
+
+// Получить поля ввода товаров, времени и расчета бюджета
+var inputTovar = document.querySelector('#items');
+ 	inputTime = document.querySelector('#time');
+	inputbudget = document.querySelector('#budget');
+
+// Получить поля имен сотрудников
+var namePersons = document.querySelectorAll('.hire-employers-item');
+for ( let i=0; i < namePersons.length; i++ ) {
+	var namePersona = namePersons[i];
+}
